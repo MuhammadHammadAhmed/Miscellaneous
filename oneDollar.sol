b471b4a0553eee7e193dd5fb047f4ee540373753
MuhammadHammadAhmed <hammad.tco@gmail.com>
2021-05-10 12:01:52 +0500

    changed back to bsc pancakeswap

--- oneDollar.sol
+++ oneDollar.sol
@@ -740,7 +740,7 @@ contract OneDollar is Context, IERC20, Ownable {
         _rOwned[_msgSender()] = _rTotal;
         
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
-         // Create a uniswap pair for this new token
+         // Create a uniswap pair for this new token-
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
 
